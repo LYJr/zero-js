2005ac5cf1aee230fb826deb6bf36857677cd9bb
이예지 <yeaji.lee@nhntoast.com>
2021-01-26 12:07:08 +0900

    feat/21.1.19. thhp Method 구현 과제 완료

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const HOST = 3000;
 const NOT_FOUND = "No such data";
 const SUCCESS = "success";
 
-let userList = new HashMap();
+let userList = new Map();
 const user = {
   id: "",
   name: "",
@@ -18,56 +18,67 @@ app.listen(HOST, IP, function () {
   console.log("server listen at localhost:" + HOST);
 });
 
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//todo body.id값 undefined 확인 중
 app.post("/users", function (req, res) {
-  // user.id = req.body.id;
-  // user.name = req.body.name;
-  // user.twitterId = req.body.twitterId;
+  user.id = req.body.id;
+  user.name = req.body.name;
+  user.twitterId = req.body.twitterId;
 
-  console.log(req.body.id);
-  // userList.set(req.body.id, user);
-
-  console.log("test");
+  userList.set(req.body.id, user);
 
   res.send(SUCCESS);
 });
 
 app.get("/users", function (req, res) {
+  var list = [];
+
   if (userList.size > 0) {
-    return res.send(userList);
+    for (let index = 1; index <= userList.size; index++) {
+      list.push(userList.get(index));
+    }
+    return res.send(list);
   }
   res.send(NOT_FOUND);
 });
 
 app.get("/users/:id", function (req, res) {
-  if (userList.get(req.params.id) !== undefined) {
-    return res.send(userList.get(req.params.id));
+  if (userList.get(Number(req.params.id)) !== undefined) {
+    return res.send(userList.get(Number(req.params.id)));
   }
   res.send(NOT_FOUND);
 });
 
 //todo code 구현
 app.put("/users/:id", function (req, res) {
-  if (userList.get(req.params.id) !== undefined) {
+  if (userList.get(Number(req.params.id)) !== undefined) {
+    user.id = req.body.id;
+    user.name = req.body.name;
+    user.twitterId = req.body.twitterId;
+
+    userList.set(Number(req.params.id), user);
+
+    return res.send(userList.get(Number(req.params.id)));
   }
   res.send(NOT_FOUND);
 });
 
 //todo code 구현
 app.patch("/users/:id", function (req, res) {
-  if (userList.get(req.params.id) !== undefined) {
-    return res.send(userList.get(req.params.id));
+  if (userList.get(Number(req.params.id)) !== undefined) {
+    userList.get(Number(req.params.id)).name = req.body.name;
+
+    console.log(userList.get(Number(req.params.id)));
+
+    return res.send(userList.get(Number(req.params.id)));
   }
   res.send(NOT_FOUND);
 });
 
-//todo test 진행
-app.delete("/users/:id", function (req, res) {
-  if (userList.get(req.params.id) !== undefined) {
-    userList.delete(req.params.id);
+app.delete("/users", function (req, res) {
+  if (userList.get(req.body.id) !== undefined) {
+    userList.delete(req.body.id);
+    return res.send(SUCCESS);
   }
   res.send(NOT_FOUND);
 });
